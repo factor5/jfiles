65248fa3ea229fd9a197fc6c6a3bdb4e056eef5a
svelikov <svelikov@4113cf7a-070c-11de-9036-69e38a880166>
2009-03-02 09:55:33 +0000

    initial commit

---
+++ trunk/Subtitle  *.srt files modifier/src/proj/subs/Start.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package proj.subs;
+
+
+/**
+ * @author Svilen Velikov
+ * 
+ * 20.01.2009
+ */
+public class Start {
+
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+	SubsModifier sm = new SubsModifier();
+	sm.modifyFiles();
+    }
+}
