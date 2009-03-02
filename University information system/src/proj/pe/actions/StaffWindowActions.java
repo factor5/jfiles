56aa9b6dd17dc3da5dba9b2b31792747a879ddbb
svelikov <svelikov@4113cf7a-070c-11de-9036-69e38a880166>
2009-03-02 10:44:21 +0000

    initial commit

---
+++ University information system/src/proj/pe/actions/StaffWindowActions.java
@@ -0,0 +1,172 @@
+/**
+ * 
+ */
+package proj.pe.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+
+import proj.pe.dto.Staff;
+import proj.pe.ui.MainWindow;
+import proj.pe.ui.StaffWindow;
+import proj.pe.util.FileReadWrite;
+import proj.pe.util.Validator;
+
+/**
+ * Action class for the staff window interface.
+ */
+public class StaffWindowActions extends WindowAdapter implements ActionListener {
+
+    /**
+     * Reference to the staff window
+     */
+    private StaffWindow staffWin;
+
+    /**
+     * Reference to the main window
+     */
+    private MainWindow mainWin;
+
+    /**
+     * List with staff data
+     */
+    private List<Staff> staffList;
+
+    /**
+     * Constructor that initializes some fields.
+     * 
+     * @param twin
+     * @param mwin
+     */
+    public StaffWindowActions(StaffWindow twin, MainWindow mwin) {
+	staffWin = twin;
+	mainWin = mwin;
+	staffList = FileReadWrite.staffList;
+    }
+
+    /**
+     * Action method that provides response for user's activities.
+     * 
+     * @param ae
+     *                event that this method catches
+     */
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+	// button save is pressed
+	if (ae.getActionCommand().equals("save")) {
+	    // call method to collect data from the interface controls
+	    Staff staff = collectAndCheckData();
+	    if (staff != null) {
+		// call method to check if provided from the user data already
+		// exists in the list
+		if (exists(staff)) {
+		    // we leave the method
+		    staffWin.displayMessage("Съществуващо ЕГН!");
+		    return;
+		}
+		// add the record for the new staff in the list
+		staffList.add(staff);
+		// call the method that stores the list in a file
+		if (FileReadWrite.storeListInFile(3)) {
+		    // if data is successfully stored we show message
+		    staffWin.displayMessage("Данните са въведени!");
+		    // disable buttons
+		    switchButtons(false);
+		}
+	    }
+	    // button reset is pressed
+	} else if (ae.getActionCommand().equals("reset")) {
+	    // reset the form fields
+	    staffWin.resetForm();
+	    // enable the buttons
+	    switchButtons(true);
+	    // button cancel is pressed
+	} else if (ae.getActionCommand().equals("cancel")) {
+	    // reset the form fields
+	    staffWin.resetForm();
+	    // enable buttons
+	    switchButtons(true);
+	    // hide this window and show the main one
+	    hideWindow();
+	}
+    }
+
+    /**
+     * Searches the list if the data provided from user exists.
+     * 
+     * @param staff
+     *                the staff object to check if exists
+     * @return true if exists or false if doesn't
+     */
+    private boolean exists(Staff staff) {
+	// iterate the list
+	for (Staff current : staffList) {
+	    // check the data
+	    if (current.getEgn().equals(staff.getEgn())) {
+		// if match occurs we return
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    /**
+     * Collects the data from the form fields and store it in a new staff
+     * object.
+     * 
+     * @return the staff object filled with new data
+     */
+    private Staff collectAndCheckData() {
+	Staff staff = new Staff();
+	String name = staffWin.fldName.getText();
+	String egn = staffWin.fldEGN.getText();
+	String phone = staffWin.fldPhoneNumber.getText();
+	if (!Validator.validateName(name)
+		|| !Validator.validateEgnOrFacNumber(egn, 1)
+		|| !Validator.validatePhoneNumber(phone)) {
+	    return null;
+	}
+	// set the collected data in the appropriate fields
+	staff.setStaffName(name);
+	staff.setEgn(egn);
+	staff.setPosition((String) staffWin.cmbPosition.getSelectedItem());
+	staff.setPhoneNumber(phone);
+	// return the prepared staff object
+	return staff;
+    }
+
+    /**
+     * Catches the window closing event so to be able to do some cleaning work
+     * before closing.
+     */
+    @Override
+    public void windowClosing(WindowEvent e) {
+	// reset the form fields
+	staffWin.resetForm();
+	// enable the buttons
+	switchButtons(true);
+	// hide this window and show the main one
+	hideWindow();
+    }
+
+    /**
+     * Switches the current window and the main one.
+     */
+    private void hideWindow() {
+	staffWin.setVisible(false);
+	mainWin.setVisible(true);
+    }
+
+    /**
+     * Turns on/off the buttons in this window according the boolean parameter.
+     * 
+     * @param turn
+     */
+    private void switchButtons(boolean turn) {
+	staffWin.btnSave.setEnabled(turn);
+	staffWin.btnCancel.setEnabled(turn);
+    }
+}
---
+++ University information system/src/proj/pe/util/FileReadWrite.java
@@ -0,0 +1,341 @@
+package proj.pe.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import proj.pe.dto.Staff;
+import proj.pe.dto.Student;
+import proj.pe.dto.Teacher;
+
+/**
+ * This class provides some convenient methods for reading data from file and
+ * writing to a file.
+ */
+public class FileReadWrite {
+
+    /**
+     * The students file object
+     */
+    private static File studFile;
+
+    /**
+     * The teachers file object
+     */
+    private static File teachFile;
+
+    /**
+     * The students file object
+     */
+    private static File staffFile;
+
+    /**
+     * The list with students data.
+     */
+    public static List<Student> studentsList;
+
+    /**
+     * The list with teachers data.
+     */
+    public static List<Teacher> teacherList;
+
+    /**
+     * The list with staffs data.
+     */
+    public static List<Staff> staffList;
+
+    /**
+     * Constructor that checks if the needed three files exists and if not they
+     * are created. Then the three lists are created from the data read from the
+     * files.
+     * 
+     * @param fileStud
+     * @param fileTeach
+     * @param fileStaff
+     * @throws IOException
+     */
+    public FileReadWrite(String fileStud, String fileTeach, String fileStaff)
+	    throws IOException {
+	checkResourceFiles(fileStud, fileTeach, fileStaff);
+	prepareLists();
+    }
+
+    /**
+     * Checks if the required files exists and if they don't it will create
+     * them.
+     * 
+     * @param fileStud
+     * @param fileTeach
+     * @param fileStaff
+     * @throws IOException
+     */
+    private void checkResourceFiles(String fileStud, String fileTeach,
+	    String fileStaff) throws IOException {
+	studFile = new File(fileStud);
+	if (!studFile.exists()) {
+	    studFile.createNewFile();
+	}
+	teachFile = new File(fileTeach);
+	if (!teachFile.exists()) {
+	    teachFile.createNewFile();
+	}
+	staffFile = new File(fileStaff);
+	if (!staffFile.exists()) {
+	    staffFile.createNewFile();
+	}
+    }
+
+    /**
+     * Prepares the three lists as first invokes the readFile method then
+     * parseString method and finally calls the setupObjects.
+     */
+    private void prepareLists() {
+	studentsList = new ArrayList<Student>();
+	teacherList = new ArrayList<Teacher>();
+	staffList = new ArrayList<Staff>();
+	List<String> list = new ArrayList<String>();
+	// prepare the students list
+	String students = readFile(studFile).toString();
+	if (!"".equals(students)) {
+	    list = parseString(students);
+	    setupObjects(list, 1);
+	}
+
+	// prepare the teachers list
+	String teachers = readFile(teachFile).toString();
+	if (!"".equals(teachers)) {
+	    list = parseString(teachers);
+	    setupObjects(list, 2);
+	}
+
+	// prepare the staffs list
+	String staff = readFile(staffFile).toString();
+	if (!"".equals(staff)) {
+	    list = parseString(staff);
+	    setupObjects(list, 3);
+	}
+    }
+
+    /**
+     * Creates objects from required type fills them with data and puts them in
+     * the list.
+     * 
+     * @param list
+     *                the list with data given as strings
+     * @param type
+     *                the type of the objects that should be created
+     */
+    private void setupObjects(List<String> list, int type) {
+	switch (type) {
+	case 1:
+	    for (String string : list) {
+		String[] fields = string.split(",");
+		Student stud = new Student();
+		stud.setStudentName(fields[0]);
+		stud.setFacNumber(fields[1]);
+		stud.setSpeciality(fields[2]);
+		stud.setCourse(Integer.valueOf(fields[3]));
+		stud.setResult(Float.valueOf(fields[4]));
+		studentsList.add(stud);
+	    }
+	    break;
+	case 2:
+	    for (String string : list) {
+		String[] fields = string.split(",");
+		Teacher teacher = new Teacher();
+		teacher.setTeachName(fields[0]);
+		teacher.setEgn(fields[1]);
+		teacher.setDepartment(fields[2]);
+		teacher.setFaculty(fields[3]);
+		teacher.setRoomNumber(fields[4]);
+		teacherList.add(teacher);
+	    }
+	    break;
+	case 3:
+	    for (String string : list) {
+		String[] fields = string.split(",");
+		Staff staff = new Staff();
+		staff.setStaffName(fields[0]);
+		staff.setEgn(fields[1]);
+		staff.setPosition(fields[2]);
+		staff.setPhoneNumber(fields[3]);
+		staffList.add(staff);
+	    }
+	    break;
+	default:
+	    break;
+	}
+    }
+
+    /**
+     * Splits the given source string around the '\r\n' signs and turns the
+     * result string array into a list.
+     * 
+     * @param source
+     *                the source string
+     * @return a list with strings that represents data
+     */
+    private List<String> parseString(String source) {
+	String[] stringObjects = source.split("\r\n");
+	List<String> list = Arrays.asList(stringObjects);
+	return list;
+    }
+
+    /**
+     * Iterates the provided list and stores its content to a file.
+     * 
+     * @param type
+     *                the type of the objects that are stored in the list
+     * @return true if the data is stored successfully
+     */
+    public static boolean storeListInFile(int type) {
+	File path = null;
+	List list = null;
+	if (type == 1) {
+	    path = studFile;
+	    list = new ArrayList<Student>();
+	    list = studentsList;
+	} else if (type == 2) {
+	    path = teachFile;
+	    list = new ArrayList<Teacher>();
+	    list = teacherList;
+	} else {
+	    path = staffFile;
+	    list = new ArrayList<Staff>();
+	    list = staffList;
+	}
+	if (list == null || list.size() == 0) {
+	    return false;
+	}
+	BufferedWriter bw = null;
+	try {
+	    // create the output stream
+	    bw = new BufferedWriter(new FileWriter(path));
+	    String string = null;
+	    // iterate the list
+	    for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+		switch (type) {
+		case 1:
+		    // get the next data from the list
+		    Student student = (Student) iterator.next();
+		    // call toString so to turn the object to string type
+		    string = student.toString();
+		    break;
+		case 2:
+		    Teacher teacher = (Teacher) iterator.next();
+		    string = teacher.toString();
+		    break;
+		case 3:
+		    Staff staff = (Staff) iterator.next();
+		    string = staff.toString();
+		    break;
+		default:
+		    break;
+		}
+		// write the result string in the file
+		bw.write(string);
+		// put a new line to separate the data
+		bw.newLine();
+	    }
+	    // if everything is okay we return true
+	    return true;
+	} catch (IOException e) {
+	    // if any error occurs we return false
+	    return false;
+	} finally {
+	    // we close the stream in the finally clause
+	    try {
+		if (bw != null) {
+		    bw.close();
+		}
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    /**
+     * Reads some data from file and appends it to a buffer.
+     * 
+     * @param path
+     * @return
+     */
+    public StringBuilder readFile(File path) {
+	if (path == null) {
+	    return null;
+	}
+	BufferedReader br = null;
+	StringBuilder result = null;
+	result = new StringBuilder();
+	int ch = 0;
+	try {
+	    br = new BufferedReader(new FileReader(path));
+	    while ((ch = br.read()) != -1) {
+		result.append((char) ch);
+	    }
+	    return result;
+	} catch (IOException e) {
+	    return null;
+	} finally {
+	    try {
+		if (br != null) {
+		    br.close();
+		}
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    /**
+     * @return the studentsList
+     */
+    public List<Student> getStudentsList() {
+	return studentsList;
+    }
+
+    /**
+     * @param studentsList
+     *                the studentsList to set
+     */
+    public void setStudentsList(List<Student> studentsList) {
+	this.studentsList = studentsList;
+    }
+
+    /**
+     * @return the teacherList
+     */
+    public List<Teacher> getTeacherList() {
+	return teacherList;
+    }
+
+    /**
+     * @param teacherList
+     *                the teacherList to set
+     */
+    public void setTeacherList(List<Teacher> teacherList) {
+	this.teacherList = teacherList;
+    }
+
+    /**
+     * @return the staffList
+     */
+    public List<Staff> getStaffList() {
+	return staffList;
+    }
+
+    /**
+     * @param staffList
+     *                the staffList to set
+     */
+    public void setStaffList(List<Staff> staffList) {
+	this.staffList = staffList;
+    }
+}
